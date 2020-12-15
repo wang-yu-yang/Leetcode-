288264831474231420bdcd355879f8ff901b1da0
wang-yu-yang <53926029+wang-yu-yang@users.noreply.github.com>
2020-12-15 17:15:08 +0800

    Update 二叉树的前序遍历.cpp

--- 二叉树的前序遍历.cpp
+++ 二叉树的前序遍历.cpp
@@ -11,11 +11,11 @@
  */
 class Solution {
 public:
-    void preorder(TreeNode* root, vector<int>& data)
+    void preorder(TreeNode* root, vector<int>& data)               //前序遍历，创建无返回值函数
     {
         if(root == nullptr)
             return;
-        data.push_back(root->val);
+        data.push_back(root->val);                         //参考算法与数据结构书籍，先打印节点，再遍历左子树，最后遍历右子树
         preorder(root->left,data);
         preorder(root->right, data);
     }
@@ -24,7 +24,7 @@ public:
 
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> data;
-        preorder(root,data);  
+        preorder(root,data);                     //调用preorder函数
         return data;      
     }
 };
